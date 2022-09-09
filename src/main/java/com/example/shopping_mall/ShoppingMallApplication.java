d9a1526732ed4f0d7353411d3ded6cc59dae67d3
DongDongShin <tlsehdgk87@naver.com>
2022-09-09 15:37:01 +0900

    Initial commit

---
+++ src/main/java/com/example/shopping_mall/ShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopping_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shopping_mall/ShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopping_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
