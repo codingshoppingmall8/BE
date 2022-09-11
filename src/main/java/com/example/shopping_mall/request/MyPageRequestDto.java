60d877bf3302c699c3b81bde6cd44eea77c954f4
scarlet0o0 <ghd7673@naver.com>
2022-09-11 18:58:37 +0900

    Feat: 마이페이지를 주고받는 Dto 추가

---
+++ src/main/java/com/example/shopping_mall/request/MyPageRequestDto.java
@@ -0,0 +1,19 @@
+package com.example.shopping_mall.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MyPageRequestDto {
+
+    private String name;
+
+    private String address;
+
+    private Integer phone;
+}
---
+++ src/main/java/com/example/shopping_mall/response/MyPageResponseDto.java
@@ -0,0 +1,21 @@
+package com.example.shopping_mall.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MyPageResponseDto {
+
+    private String email;
+
+    private String name;
+
+    private String address;
+
+    private Integer phone;
+}
