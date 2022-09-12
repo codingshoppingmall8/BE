290729def6f33d2c729c8fe2f9a89c746413fb58
DongDongShin <tlsehdgk87@naver.com>
2022-09-12 15:57:52 +0900

    add Builder Annotation

--- src/main/java/com/example/shopping_mall/request/RequestPostDto.java
+++ src/main/java/com/example/shopping_mall/request/RequestPostDto.java
@@ -2,6 +2,7 @@ package com.example.shopping_mall.request;
 
 import com.example.shopping_mall.entity.Category;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,13 +11,12 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Builder
 public class RequestPostDto {
     private String imgUrl;
     private String title;
     private String desc;//설명
-    private String manual;//상세 정보
-    private int  cost;//point
+    private int  cost;//가격 주문
     private int  point;//point
     private Category category;
 }
