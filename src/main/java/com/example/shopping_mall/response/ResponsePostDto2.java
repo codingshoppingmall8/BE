b169fef16164de785900b4851716bccc105be3ad
DongDongShin <tlsehdgk87@naver.com>
2022-09-12 15:59:23 +0900

    add dtoclass

---
+++ src/main/java/com/example/shopping_mall/response/ResponsePostDto2.java
@@ -0,0 +1,25 @@
+package com.example.shopping_mall.response;
+
+
+
+import com.example.shopping_mall.entity.Category;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResponsePostDto2 {
+    private String imgUrl;
+    private String title;
+    private String desc;//설명
+    private int  cost;//가격 주문
+    private int  cnt;//가격 주문
+    private int  point;//point
+
+}
