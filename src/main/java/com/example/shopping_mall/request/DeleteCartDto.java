5b2d4e499673f85ce8c4bc0ad9cc29901a5d952c
DongDongShin <tlsehdgk87@naver.com>
2022-09-14 03:34:12 +0900

    Add: 지울 장바구니의 id를 받는 dto생성

---
+++ src/main/java/com/example/shopping_mall/request/DeleteCartDto.java
@@ -0,0 +1,18 @@
+package com.example.shopping_mall.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DeleteCartDto {
+    private List<Long> postId = new ArrayList<>();
+}
